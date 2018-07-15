c07d0ae35e44a19f270a02e24ec70f584dd29af3
cotgyu <v123v123s@naver.com>
2018-07-15 17:43:50 +0900

    오류 페이지 설정(403, 404, 405, 500)+(시큐리티- 권한 접근 실패 핸들러 수정_메시지 출력 ->> 403페이지)

--- WPP/src/main/java/com/wpp/security/AccessFailureHandler.java
+++ WPP/src/main/java/com/wpp/security/AccessFailureHandler.java
@@ -3,6 +3,7 @@ package com.wpp.security;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
+//일반사용자가 관리자 권한 페이지 이동시 처리 
 public class AccessFailureHandler implements AccessDeniedHandler {
 	private static final Logger logger = LoggerFactory.getLogger(AccessFailureHandler.class);
 	private String errorPage;
@@ -35,15 +37,9 @@ public class AccessFailureHandler implements AccessDeniedHandler {
 	@Override
 	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception)
 			throws IOException, ServletException {
-		String accept = request.getHeader("accept");
-		String error = "true";
-		String message = exception.getMessage();
-		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-		response.setCharacterEncoding("EUC-KR");
-		String data = StringUtils.join(new String[] { "권한이 없습니다. 정상적인 경로로 이용해주세요! " });
-		PrintWriter out = response.getWriter();
-		out.print(data);
-		out.flush();
-		out.close();
+	//권한에 맞지않는 접근 시도 시  403으로 처리 
+		RequestDispatcher Rd = request.getRequestDispatcher("/error/403");
+		Rd.forward(request, response);
+		
 	}
 }
---
+++ WPP/src/main/webapp/WEB-INF/views/commons/error404.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Cot :: ERROR </title>
+
+<%@ include file="../commons/_header.jspf" %>
+</head>
+<body>
+<%@ include file="../commons/_top.jspf" %>
+	<div class="center">
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+			<br><br><br><br><br>
+				<H2>처리 중 에러가 발생하였습니다.</H2>
+				<H2>올바른 경로로 접근해주세요!</H2> 
+				
+			</div>
+		</div>
+	</div>
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<%@ include file="../commons/_foot.jspf"%>
+	</div>
+</body>
+</html>
