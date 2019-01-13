00b5e66fee0837db3b0a83c875f0b316d431f150
cotgyu <v123v123s@gmail.com>
2019-01-13 16:55:19 +0900

    @Transactional 간단하게 실습해보기

--- WPP/src/main/java/com/wpp/service/FreeBoardService.java
+++ WPP/src/main/java/com/wpp/service/FreeBoardService.java
@@ -1,60 +1,44 @@
 package com.wpp.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import com.wpp.controller.HomeController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wpp.dao.FreeBoardDao;
 import com.wpp.dao.UserDao;
 import com.wpp.domain.FreeBoard;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class FreeBoardService {
+public interface FreeBoardService {
 	
-	@Autowired
-	FreeBoardDao freeboardDao;
-	@Autowired
-	UserDao userDao;
-	
-	public int countboard(String searchOption, String keyword) throws Exception {
-		return freeboardDao.countboard(searchOption, keyword);
-	}
+	public int countboard(String searchOption, String keyword) throws Exception ;
 
-	public List<FreeBoard> Viewlist(int start, int end, String searchOption, String keyword) throws Exception {
-		return freeboardDao.Viewlist(start, end, searchOption, keyword);
-	}
 
-	public List<FreeBoard> popboard() throws Exception {
-		return freeboardDao.popboard();
-	}
+	public List<FreeBoard> Viewlist(int start, int end, String searchOption, String keyword) throws Exception ;
 
-	public void create(FreeBoard vo) throws Exception {
-		freeboardDao.create(vo);	
-	}
+	public List<FreeBoard> popboard() throws Exception ;
 
-	public void uphit(int bnum) throws Exception {
-		freeboardDao.uphit(bnum);
-	}
+	public void create(FreeBoard vo) throws Exception ;
 
-	public String findByWriter(int bnum) {
-		return freeboardDao.findByWriter(bnum);
-	}
+	public void uphit(int bnum) throws Exception;
 
-	public Object read(int bnum) throws Exception {
-		return freeboardDao.read(bnum);
-	}
+	public String findByWriter(int bnum) ;
 
-	public FreeBoard detail(Integer bnum) {		
-		return freeboardDao.detail(bnum);
-	}
 
-	public void update(FreeBoard vo) throws Exception {
-		freeboardDao.update(vo);
-	}
+	@Transactional
+	public Object read(int bnum) throws Exception;
 
-	public void delete(int bnum) throws Exception {
-		freeboardDao.delete(bnum);
-	}
+	public FreeBoard detail(Integer bnum);
+
+
+	public void update(FreeBoard vo) throws Exception;
+
+	public void delete(int bnum) throws Exception ;
 
 }
